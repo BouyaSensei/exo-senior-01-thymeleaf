0cc7d5b6f6805090fe4cbc03b2314ff8090eef1d
BouyaSensei <buildario@outlook.fr>
2026-06-17 02:55:02 +0200

    Mise à jour du contrôleur de tableau de bord pour utiliser l'objet User et modification du modèle HTML pour afficher le nom d'utilisateur.

--- src/main/java/com/security/controllers/DashBoardController.java
+++ src/main/java/com/security/controllers/DashBoardController.java
@@ -4,12 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.security.records.User;
+
 @Controller
 public class DashBoardController {
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
-        String username = "Axel";
-        model.addAttribute("username", username);
+        // String username = "Axel";
+        User myUser = new User("Axel", "ROLE_ADMIN");
+        model.addAttribute("user", myUser);
         return "dashboard";
     }
 }
