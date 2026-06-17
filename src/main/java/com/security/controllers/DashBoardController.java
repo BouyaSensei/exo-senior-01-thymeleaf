a140846c8567fc1127dcd17c11665074118f117a
BouyaSensei <buildario@outlook.fr>
2026-06-17 17:17:50 +0200

    Ajout de la dépendance spring-boot-starter-security et mise à jour du contrôleur de tableau de bord pour gérer l'authentification de l'utilisateur.

--- src/main/java/com/security/controllers/DashBoardController.java
+++ src/main/java/com/security/controllers/DashBoardController.java
@@ -1,5 +1,12 @@
 package com.security.controllers;
 
+import java.util.List;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +19,10 @@ public class DashBoardController {
     public String dashboard(Model model) {
         // String username = "Axel";
         User myUser = new User("Axel", "ROLE_ADMIN");
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
         model.addAttribute("user", myUser);
+        Authentication auth = new UsernamePasswordAuthenticationToken("Axel", null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(auth);
         return "dashboard";
     }
 }
